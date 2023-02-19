8580284796af676ff59a3625236d694da5388db8
rumeshrath97 <g9673.rumesh@gmail.com>
2023-02-20 02:27:28 +0530

    Finalized Service class

---
+++ src/main/java/com/example/restapi/service/UserService.java
@@ -0,0 +1,42 @@
+package com.example.restapi.service;
+
+import com.example.restapi.dto.UserDTO;
+import com.example.restapi.entity.User;
+import com.example.restapi.repo.UserRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public UserDTO saveUser(UserDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+    public List<UserDTO> getAllUsers(){
+        List<User>userList=userRepo.findAll();
+        return modelMapper.map(userList,new TypeToken<List<UserDTO>>(){}.getType());
+    }
+
+    public UserDTO updateUser(UserDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO,User.class));
+        return userDTO;
+    }
+
+    public boolean deleteUser(UserDTO userDTO){
+        userRepo.delete(modelMapper.map(userDTO,User.class));
+        return true;
+    }
+
+
+}
